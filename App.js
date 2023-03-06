a5e8a902224e45155ab61bff2722d50a993c62e1
jefercanya <jefercanya@hotmail.com>
2023-03-06 14:48:59 -0500

    antes de revertir

--- App.js
+++ App.js
@@ -3,13 +3,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from "./src/navigation/NavigationStack";
+import NavigationStack2 from "./src/navigation/NavigationStack2";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <NavigationStack />
-    </NavigationContainer>
-  );
+  if(2 == 1){
+    console.log("Si funciona");
+    return (
+      <NavigationContainer>
+        <NavigationStack2/>
+      </NavigationContainer>
+    );
+  }else{
+    return (
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+    );
+  }
+ 
 }
 
 const styles = StyleSheet.create({
--- src/screens/WebView.js
+++ src/screens/WebView.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import {WebView} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function SettingsScreen(props) {
 
     return (
         <WebView
         source={{
-          uri: 'https://github.com/facebook/react-native',
+          uri: 'https://micontadorbot.azurewebsites.net',
         }}
-        style={{marginTop: 20}}
+        style={{marginTop: 0}}
       />
     );
 }
